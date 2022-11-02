67c76dd6469427bf0652e3f389303dc8db5ae106
Quintin <quintinhull@carfax.com>
2022-11-01 19:50:51 -0500

    error handling set up for api routes

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const routes = require('./routes');
-
+const { ValidationError } = require('sequelize');
 const { environment } = require('./config');
 const isProduction = environment === 'production';
 
@@ -18,22 +18,52 @@ app.use(express.json());
 
 if (!isProduction) {
     app.use(cors());
+}
+app.use(helmet({
+  contentSecurityPolicy: false
+}));
+
+// BOOKMARK
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true,
+    },
+  })
+);
+
+
+app.use(routes)
+
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = ["The requested resource couldn't be found."];
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = 'Validation error';
   }
-  app.use(helmet({
-    contentSecurityPolicy: false
-  }));
-  
-  // BOOKMARK
-  app.use(
-    csurf({
-      cookie: {
-        secure: isProduction,
-        sameSite: isProduction && "Lax",
-        httpOnly: true,
-      },
-    })
-  );
+  next(err);
+});
 
-  app.use(routes)
+// Error formatter
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack,
+  });
+});
 
-  module.exports = app;
+module.exports = app;
