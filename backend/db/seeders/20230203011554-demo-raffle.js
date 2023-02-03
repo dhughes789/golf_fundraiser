8c6e4c85486d50e964d0e185c1470ca800d9b4ac
Quintin <quintinhull@carfax.com>
2023-02-02 19:35:48 -0600

    formatting

--- backend/db/seeders/20230203011554-demo-raffle.js
+++ backend/db/seeders/20230203011554-demo-raffle.js
@@ -14,15 +14,15 @@ module.exports = {
         donor: "Rod's Big Ole Fish"
       },
       {
-        imageUrl: 'https://ibb.co/99wCqQ2',
-        description: 'This is another description of a grill for grilling grilled foods.',
-        donor: 'Schrimpf Landscaping Incorporated'
-        },
-        {
-          imageUrl: 'https://ibb.co/h13b2w0',
-          description: 'This is another description of tickets to a St. Louis Cardinals baseball game.',
-          donor: "Rod's Big Ole Fish"
-        }
+      imageUrl: 'https://ibb.co/99wCqQ2',
+      description: 'This is another description of a grill for grilling grilled foods.',
+      donor: 'Schrimpf Landscaping Incorporated'
+      },
+      {
+        imageUrl: 'https://ibb.co/h13b2w0',
+        description: 'This is another description of tickets to a St. Louis Cardinals baseball game.',
+        donor: "Rod's Big Ole Fish"
+      }
     ], {});
   },
 
