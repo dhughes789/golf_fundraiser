2ee8030916975a87a5ad4b51680dfd32e1c7d853
dhughes789 <dantonhughes@gmail.com>
2023-10-20 16:06:53 -0500

    added username, email,shirt,password,confirm password,submit button to signup page

--- frontend/src/components/SignupFormPage/index.js
+++ frontend/src/components/SignupFormPage/index.js
@@ -2,9 +2,27 @@ import React, { useState } from 'react';
 import * as sessionActions from '../../store/session';
 import { useDispatch, useSelector } from 'react-redux';
 import { Redirect } from 'react-router-dom';
-const [username, setUsername] = useState('');
 
 function SignupFormPage(){
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [shirt, setShirt] = useState('L');
+    const [password, setPassword] = useState('');
+    const [confirmpassword, setConfirmPassword] = useState('');
+    
+    const isDisabled = ()=>{
+        if(password == confirmpassword && password.length>7 && password.length<25){
+            console.log('work');
+            return false;
+            
+        }
+        else{
+            console.log('no worked');
+            return true;
+        }
+     }
+    
     return (
         <form>
             <input
@@ -15,6 +33,48 @@ function SignupFormPage(){
                 placeholder='username'
                 required
             />
+            <input
+                className='signup-form__input'
+                type="text"
+                value={email}
+                onChange={(e)=>setEmail(e.target.value)}
+                placeholder='email'
+                required
+            />
+            <input
+                className='signup-form__input'
+                type="text"
+                value={phone}
+                onChange={(e)=>setPhone(e.target.value)}
+                placeholder='###-###-####'
+            />
+            <select className='signup-form__input' value={shirt} onChange={(e)=>setShirt(e.target.value)}>
+            <option value="S">Small</option>
+            <option value="M">Medium</option>
+            <option value="L">Large</option>
+            <option value="XL">XL</option>
+            <option value="XXL">XXL</option>
+            <option value="XXXL">XXXL</option>
+          </select>
+          <input
+                className='signup-form__input'
+                type="password"
+                value={password}
+                onChange={(e)=>setPassword(e.target.value)}
+                placeholder='password'
+                required
+            />
+          <input
+                className='signup-form__input'
+                type="password"
+                value={confirmpassword}
+                onChange={(e)=>setConfirmPassword(e.target.value)}
+                placeholder='confirm password'
+                required
+            />
+            <button className='signup-form__input' type='submit' disabled={isDisabled()} >SUBMIT
+
+            </button>
 
         </form>
     )
