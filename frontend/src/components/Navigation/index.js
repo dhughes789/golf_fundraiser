6800268b1e62c0d70ab8eacd45b5805b28687669
dhughes789 <dantonhughes@gmail.com>
2024-01-26 15:55:06 -0600

    ticker library

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import { useSelector } from "react-redux";
-import ProfileButton from "./ProfileButton";
-import "./Navigation.css";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import ProfileButton from './ProfileButton';
+import './Navigation.css';
 
 function Navigation({ isLoaded }) {
   const sessionUser = useSelector((state) => state.session.user);
@@ -13,23 +13,31 @@ function Navigation({ isLoaded }) {
   } else {
     //will be drop down
     sessionLinks = (
-      <div className="navbar_session">
-        <NavLink to="/login">Log In</NavLink>
-        <NavLink to="/signup">Sign Up</NavLink>
+      <div className='navbar_session'>
+        <NavLink to='/login'>Log In</NavLink>
+        <NavLink to='/signup'>Sign Up</NavLink>
       </div>
     );
   }
 
   return (
-    <div className="navbar_container">
+    <div className='navbar_container'>
       <div>
-        <NavLink exact to="/" className="navbar_home">
+        <div className='container' onClick={() => myFunction(this)}>
+          <div className='bar1'></div>
+          <div className='bar3'></div>
+        </div>
+        <NavLink exact to='/' className='navbar_home'>
           Home
         </NavLink>
       </div>
       <div>{isLoaded && sessionLinks}</div>
     </div>
   );
+}
+
+function myFunction(x) {
+  x.classList.toggle('change');
 }
 
 export default Navigation;
