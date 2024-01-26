169b99fa8e2fb6dbd39c1dd8c21856bb361aa31d
Quintin <quintinhull@carfax.com>
2024-01-26 16:39:33 -0600

    working on sponsor ticker

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -6,7 +6,11 @@ import "./Footer.css";
 function Footer({ user }) {
   return (
     <div className="footer_container">
-      <div>footer placeholder</div>
+      <div className="footer_column">Sponsors:</div>
+      <div className="footer_column">
+        <h1 class="words">Nike, Adidas, Fila, Titleist, Jofa</h1>
+      </div>
+      <div className="footer_column">placeholder</div>
     </div>
   );
 }
