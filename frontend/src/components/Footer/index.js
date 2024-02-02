b978c9d2b72dd6173b1e1fc76fac81486721f266
Quintin <quintinhull@carfax.com>
2024-02-02 17:00:22 -0600

    need to pull actual sponsors via dispatch

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -1,10 +1,27 @@
-import React, { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
-import * as sessionActions from "../../store/session";
+import React from "react";
 import "./Footer.css";
 
 function Footer({ user }) {
   const sponsorList = [
+    "Thank you to all our sponsors!",
+    "Front Row Films",
+    "Play It Again Sports",
+    "Ghetto Gerbes",
+    "Applebees",
+    "Jamaican Jerk Hut",
+    "Helias Catholic",
+    "Central Bank",
+    "Capital Holdings",
+    "Test",
+    "Front Row Films",
+    "Play It Again Sports",
+    "Ghetto Gerbes",
+    "Applebees",
+    "Jamaican Jerk Hut",
+    "Helias Catholic",
+    "Central Bank",
+    "Capital Holdings",
+    "Test",
     "Front Row Films",
     "Play It Again Sports",
     "Ghetto Gerbes",
@@ -17,13 +34,12 @@ function Footer({ user }) {
   ];
 
   const renderSponsorList = (sponsors) => {
-    return sponsors.join(" |  ");
+    return sponsors.join(" • ");
   };
 
   return (
     <div className="footer_container">
       <div className="footer_sponsor">
-        <div className="footer_sponsor__title">Sponsors: </div>
         <div className="footer_sponsor__ticker">
           <div class="footer_sponsor__sponsors">
             {renderSponsorList(sponsorList)}
